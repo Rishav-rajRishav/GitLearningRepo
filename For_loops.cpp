757ba002fa3089a883c5624556be612483fbd951
Rishav Raj <er.raj.rishav@gmail.com>
2021-08-21 17:14:47 +0530

    For loops updated with new alpha

---
+++ For_loops.cpp
@@ -0,0 +1,11 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	for(int i=0; i<5; i++){
+		cout << i << "Hello" << endl;
+	}
+	cout << "Done with for loops" << endl;
+	cout << "hii";
+}
